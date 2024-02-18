4bbdb92c35084a0df0a6d8fdecb51c8fdd7ff313
Pierreand <andrieu.ps@gmail.com>
2024-02-18 14:30:06 +0100

    fixe

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
 const Navbar = () => {
+
+  
+
+  function replayService(n) {
+
+     if (n > 6) {
+      console.log("alright")
+     } else {
+      console.log("lesssss")
+     }
+  }
+  console.log(replayService(5))
   return (
     <div >
       <nav>
